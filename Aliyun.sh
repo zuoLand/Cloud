05f59928aace113455307089c7aacb39117e36e9
zuoLand <chunyuan2008@163.com>
2017-12-23 17:43:56 +0800

    Add Description on Maven

--- Aliyun.sh
+++ Aliyun.sh
@@ -163,5 +163,21 @@
 #Nginx安装配置
 #http://blog.csdn.net/hanimashi/article/details/53428594
 
+#maven安装
+#https://www.cnblogs.com/bincoding/p/6156236.html
+#cd /usr/local/src/
+#wget http://mirrors.hust.edu.cn/apache/maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.tar.gz
+#tar zxf apache-maven-3.1.1-bin.tar.gz
+#mv apache-maven-3.1.1 /usr/local/maven3
 
+#vi /etc/profile然后还需要 配置环境变量。
+#在适当的位置添加
+#export M2_HOME=/usr/local/maven3
+#export PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin
+ 
+#保存退出后运行下面的命令使配置生效，或者重启服务器生效。
+#source /etc/profile
+ 
+#验证版本
+#mvn -v
 
