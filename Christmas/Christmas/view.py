226208c39ea608c4f02f02f8bd6d5db2a5c657d9
svnadmin <chunyuan2008@163.com>
2017-12-25 17:13:17 +0800

    Add Person Christmas WebSite With Bootstrap And JQuery This Website base on Django.

---
+++ Christmas/Christmas/view.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+ 
+from django.http import HttpResponse
+from django.shortcuts import render
+ 
+#import Global Personal Setting Variable Parameters
+from django.conf import settings
+
+#Def Global Settging Function to get Variable Params from Setting
+def global_setting(context):
+	context['SITE_ADDRESS_PRE']=settings.SITE_ADDRESS_PRE
+	context['SITE_ADDRESS']=settings.SITE_ADDRESS
+	context['SITE_NAME']=settings.SITE_NAME
+	context['SITE_FOOTER_EMAIL']=settings.SITE_FOOTER_EMAIL
+	context['SITE_FOOTER_COMPANY']=settings.SITE_FOOTER_COMPANY
+
+
+
+def toIndex(request):
+    context          = {}
+    global_setting(context)
+
+    return render(request, 'index.html', context)
+
+def toIntroduction(request):
+    context          = {}
+
+    global_setting(context)
+
+    return render(request, 'introduction.html', context)
+
+
+def toWork(request):
+    context          = {}
+
+    global_setting(context)
+
+    return render(request, 'work.html', context)
+
+def toRef(request):
+    context          = {}
+
+    global_setting(context)
+
+    return render(request, 'ref.html', context)
