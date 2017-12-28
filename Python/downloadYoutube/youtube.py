cf8e97ab1da679dddbb83594ceacd0ae2400c9d6
zuoLand <chunyuan2008@163.com>
2017-12-28 09:51:28 +0800

    Add Python Downlad

---
+++ Python/downloadYoutube/youtube.py
@@ -0,0 +1,10 @@
+import youtube_dl
+import urllib
+import shutil
+ydl_opts = {}
+
+
+
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=pEJYmnHIPjs'])
