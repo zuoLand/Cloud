6fa8f5c2b82feb0627cb3d0b121fefe97196f944
zuoLand <chunyuan2008@163.com>
2017-12-29 23:28:42 +0800

    Update Python.sh

--- Python.sh
+++ Python.sh
@@ -1,3 +1,19 @@
 yum install MySQL-python
 
 
+
+#一、mac 安装ipython notebook
+sudo easy_install pip   
+sudo pip install --upgrade setuptools --user python  
+sudo pip install ipython[all] --user python  
+sudo pip install jupyter --user python  
+
+
+
+
+#二、mac 安装 scikit-image 包
+[plain] view plain copy
+sudo pip install -U scikit-image --user python  
+
+
+#三、运行ipython notebook
